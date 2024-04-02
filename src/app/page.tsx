17c7aa5f051b0ff5bc75eba0be9786904d727335
vivartmedia <nanno70@yahoo.de>
2024-04-01 23:08:53 -0700

    TIP CALCULATOR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,137 @@
-import Image from "next/image";
+
+'use client'
+import { Button } from "flowbite-react";
+import { Container } from "postcss";
+import { Card } from "flowbite-react";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  return (<>
+    <main className="flex min-h-screen flex-col items-center p-20 bg-LGcyan">
+
+      <header className="my-14">
+        <h1 className="space-mono-bold text-3xl text-VDcyan">S P L I</h1>
+        <h1 className="space-mono-bold text-3xl text-VDcyan">T T E R</h1>
+      </header>
+
+      <Card className="w-900px h-600px rounded-3xl overflow-hidden">
+        <div className="grid grid-cols-2 h-full ">
+          <div className="flex flex-col justify-between p-8">
+            <div>
+              <div className="mb-5 grid">
+                <label className="space-mono-bold text-xl text-VDcyan" htmlFor="">Bill</label>
+
+                <div className="relative flex items-center bg-slate-100 rounded-md hover:border-2 hover:border-Scyan">
+                  <span className="absolute left-0 pl-3 text-gray-500"></span>
+                  <input
+                    type="text"
+                    className="w-full text-2xl text-VDcyan space-mono-regular  rounded-md border-0 bg-slate-100 pl-5 pr-3 py-2 text-right hover:border-2  hover:border-Scyan"
+                    style={{
+                      backgroundImage: `url('/assets/icon-dollar.svg')`, // Using the public folder path
+                      backgroundRepeat: 'no-repeat',
+                      backgroundPosition: 'left 10px center', // Adjust as needed
+                      backgroundSize: '11px 17px', // Adjust based on the size of your icon
+                    }}
+                    placeholder="0"
+                    step="0.01"
+                    min="0"
+                    onInput={(e) => {
+                      e.currentTarget.value = e.currentTarget.value.replace(/[^0-9.]/g, '').replace(/(\..*)\..*/g, '$1')
+                    }}
+                  />
+
+                </div>
+              </div>
+            </div>
+
+            <div className="">
+              <label className="space-mono-bold text-xl text-VDcyan" htmlFor="">Select Tip %</label>
+              <div className="flex justify-between mb-4">
+                <Button className="w-28">5%</Button>
+                <Button className="w-28">10%</Button>
+                <Button className="w-28">15%</Button>
+              </div>
+
+              <div>
+                <div className="flex justify-between">
+                  <Button className="w-28">25%</Button>
+                  <Button className="w-28">50%</Button>
+                  <input className="w-28 text-xl text-VDcyan space-mono-bold  rounded-md border-0 bg-slate-100 pl-5 pr-3 py-2 text-center hover:border-2  hover:border-Scyan" type="text" placeholder="Custom"
+                    step="1"
+                    min="0"
+                    onInput={(e) => {
+                      // Only allow numbers (and optionally a single decimal point)
+                      e.currentTarget.value = e.currentTarget.value.replace(/[^0-9.]/g, '').replace(/(\..*)\..*/g, '$1');
+                    }}
+                  />
+                </div>
+              </div>
+
+            </div>
+
+            <div>
+              <div className="mb-5 grid">
+                <label className="space-mono-bold text-xl text-VDcyan" htmlFor="">Number of People</label>
+                <input
+                  type="text"
+                  className="w-full text-2xl text-VDcyan space-mono-regular  rounded-md border-0 bg-slate-100 pl-5 pr-3 py-2 text-right hover:border-2  hover:border-Scyan"
+                  style={{
+                    backgroundImage: `url('/assets/icon-person.svg')`, // Using the public folder path
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition: 'left 10px center', // Adjust as needed
+                    backgroundSize: '15px 17px', // Adjust based on the size of your icon
+                  }}
+                  onInput={(e) => {
+                    // Ensure only whole numbers greater than 0 are allowed
+                    e.currentTarget.value = e.currentTarget.value.replace(/\D/g, '').replace(/^0+/g, '');
+                  }}
+                  placeholder="0"
+                />
+              </div>
+            </div>
+          </div>
+
+
+          <div className="bg-VDcyan rounded-2xl m-4 p-">
+            {/* <div className="grid grid-cols-2 h-full "> */}
+              {/* <div className="flex flex-col justify-between p-8"> */}
+                
+                <div>
+
+                  <div className="flex justify-between w-full">
+                    <div>
+                      <div>
+                        <p className="text-xl text-slate-200">Tip Amount</p>
+                      </div>
+                      <div><p className="text-md text-Gcyan">/ person</p></div>
+                    </div>
+                    <div><p className="text-md text-Gcyan">$0.00</p></div>
+                  </div>
+
+                  <div className="flex justify-between w-full">
+                    <div>
+                      <div>
+                        <p className="text-xl text-slate-200">Tip Amount</p>
+                      </div>
+                      <div><p className="text-md text-Gcyan">/ person</p></div>
+                    </div>
+                    <div><p className="text-md text-Gcyan">$0.00</p></div>
+                  </div>
+                  
+                </div>
+                <div>
+
+                </div>
+              {/* </div> */}
+
+            {/* </div> */}
+          </div>
         </div>
-      </div>
+      </Card>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
+  </>
+
   );
 }
+
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
   theme: {
     extend: {
@@ -13,8 +14,32 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors:{
+        //Strong Cyan
+        Scyan: 'hsl(172, 67%, 45%)',
+        //Very Dark Cyan
+        VDcyan: 'hsl(183, 100%, 15%)',
+        //Dark Gray Cyan
+        DGcyan: 'hsl(186, 14%, 43%)',
+        //Grayish Cyan
+        Gcyan: 'hsl(184, 14%, 56%)',
+        //Light Grayish Cyan
+        LGcyan: 'hsl(185, 41%, 84%)',
+        //Very Light Grayish Cyan
+        VLGcyan: 'hsl(189, 41%, 97%)'
+      },
+      width: {
+        'custom': '900px', // Custom width
+        '900px': '920px',
+      },
+      height: {
+        'custom': '500px', // Custom width
+        '600px': '480px'
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require("flowbite/plugin"),
+  ],
 };
 export default config;
